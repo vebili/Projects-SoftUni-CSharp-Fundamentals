77140d71bcc7e945b23d7e788bb76a8f07b460e2
vebili <74007431+vebili@users.noreply.github.com>
2020-12-19 19:20:01 +0200

    Add project files.

---
+++ 20201213.FinalExam/01.Problem3/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace _01.Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string input;
+            while ((input = Console.ReadLine()) != "Finish")
+            {
+                string[] cmdArgs = input.Split();
+                string command = cmdArgs[0];
+                switch (command)
+                {
+                    case "Replace":
+                        message = Replace(message, cmdArgs);
+                        break;
+                    case "Cut":
+                        message = Cut(message, cmdArgs);
+                        break;
+                    case "Make":
+                        message = Make(message, cmdArgs);
+                        break;
+                    case "Check":
+                        Check(message, cmdArgs);
+                        break;
+                    case "Sum":
+                        Sum(message, cmdArgs);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        private static void Sum(string message, string[] cmdArgs)
+        {
+            int startIndex = int.Parse(cmdArgs[1]);
+            int endIndex = int.Parse(cmdArgs[2]);
+            if (startIndex >= 0 && startIndex < message.Length && endIndex < message.Length)
+            {
+                int sum = 0;
+                string current = message.Substring(startIndex, endIndex - startIndex + 1);
+                foreach (var item in current)
+                {
+                    sum += item;
+                }
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                Console.WriteLine("Invalid indexes!");
+            }
+        }
+        private static void Check(string message, string[] cmdArgs)
+        {
+            string substring = cmdArgs[1];
+            if (message.Contains(substring))
+            {
+                Console.WriteLine($"Message contains {substring}");
+            }
+            else
+            {
+                Console.WriteLine($"Message doesn't contain {substring}");
+            }
+        }
+        private static string Make(string message, string[] cmdArgs)
+        {
+            switch (cmdArgs[1])
+            {
+                case "Upper":
+                    message = message.ToUpper();
+                    break;
+                case "Lower":
+                    message = message.ToLower();
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(message);
+            return message;
+        }
+        private static string Cut(string message, string[] cmdArgs)
+        {
+            int startIndex = int.Parse(cmdArgs[1]);
+            int endIndex = int.Parse(cmdArgs[2]);
+            if (startIndex >= 0 && startIndex < message.Length && endIndex < message.Length)
+            {
+                message = message.Remove(startIndex, endIndex - startIndex + 1);
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine("Invalid indexes!");
+            }
+
+            return message;
+        }
+        private static string Replace(string message, string[] cmdArgs)
+        {
+            char current = char.Parse(cmdArgs[1]);
+            char replacement = char.Parse(cmdArgs[2]);
+            message = message.Replace(current, replacement);
+            Console.WriteLine(message);
+            return message;
+        }
+    }
+}
---
+++ 20201213.FinalExam/03.Problem1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _03.Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string regex = @"^(.+)>(?<first>[0-9]{3})\|(?<second>[a-z]{3})\|(?<third>[A-Z]{3})\|(?<fourth>[^<>]{3})<\1$";
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Match m = Regex.Match(input, regex);
+
+                if (m.Success)
+                {
+                    string password = m.Groups["first"].Value + m.Groups["second"].Value + m.Groups["third"].Value + m.Groups["fourth"].Value;
+                    Console.WriteLine($"Password: {password}");
+                }
+                else
+                {
+                    Console.WriteLine($"Try another password!");
+                }
+            }
+        }
+    }
+}
